776a6f1977ec21c77ab8353b2ffcf4ac58caac27
overmellow <morteza_faraji@yahoo.com>
2021-09-13 15:22:30 -0700

    first commit

---
+++ src/test/java/com/wv/mfaraji/exercises/springherokudemo/SpringHerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wv.mfaraji.exercises.springherokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
